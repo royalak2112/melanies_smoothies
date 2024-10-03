5d30dc704a0f94577752e29c4320a733f9fb67ba
royalak2112 <akashdawale96@gmail.com>
2024-10-03 14:43:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,12 +18,12 @@ st.write("The Name on your smoothie will be:",name_on_order)
 cnx =st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
-
-#pd_df=my_dataframe.topandas()
-#st.dataframe(pd_df)
+#st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
+
+pd_df=my_dataframe.topandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect('choose up to 5 ingredients:',my_dataframe)
 
